f0f8757702a818a85759cf1bdd821cf73784a02d
Zach Callanan <caveofmonsters@gmail.com>
2020-05-08 12:35:29 +0200

    card progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,80 @@ require 'json'
 url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
 list = open(url).read
 result = JSON.parse(list)
+drink_ids = []
+result['drinks'].each do |row|
+  drink_ids << row['idDrink']
+end
+
+Cocktail.destroy_all
+Dose.destroy_all
+
+drink_ids.each do |id|
+  url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{id}"
+  drink_details_json = open(url).read
+  drink_details = JSON.parse(drink_details_json)
+  cocktail = Cocktail.new(
+    name: drink_details['drinks'][0]['strDrink'],
+    description: drink_details['drinks'][0]['strInstructions'],
+    picture_url: drink_details['drinks'][0]['strDrinkThumb']
+  )
+  cocktail.save
+  ingredients = Ingredient.all
+  ingredient_hash = {}
+  ingredients.each do |ingredient|
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient1']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure1']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient2']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure2']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient3']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure3']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient4']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure4']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient5']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure5']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient6']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure6']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient7']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure7']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient8']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure8']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient9']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure9']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient10']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure10']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient11']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure11']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient12']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure12']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient13']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure13']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient14']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure14']]
+    end
+    if ingredient['name'] == drink_details['drinks'][0]['strIngredient15']
+      ingredient_hash[ingredient['id']] = [ingredient['name'], cocktail['strMeasure15']]
+    end
+  end
+
+  ingredient_hash.each do |key, value|
+    dose = Dose.new(
+      cocktail_id: cocktail.id,
+      ingredient_id: key,
+      description: "#{value[0]} - #{value[1]}"
+    )
+    dose.save
+  end
+end
